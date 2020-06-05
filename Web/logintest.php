4aed68777d937c88fd6cd0aa875f60fa83692fdf
liubohung <liubohung@gmail.com>
2020-06-06 01:36:42 +0800

    good

--- Web/logintest.php
+++ Web/logintest.php
@@ -3,81 +3,72 @@
 	if ( isset($_POST['account']) && isset($_POST['password'])){
 		$account = $_POST['account'];
 		$pwd = $_POST['password'];
-		$account_T = substr($account,0,1);
-		function Student(){
-			try{
-				$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
-				$cusr=$db->query("SELECT Student_id FROM student WHERE Student_id = '$account';");
-				$row1=$cusr->fetch(PDO::FETCH_BOTH);
-				if(empty($row1[0])){
-					$db=null;
-					print<<<_END
-					<script>
-					alert ("使用者不存在");
-					setTimeout(function(){window.location.href='login.php';},1000);
-					</script>
-					_END;
-				}else {
-					$cpwd=$db->query("SELECT Password FROM student WHERE Student_id = '$account';");
-					$row2=$cpwd->fetch(PDO::FETCH_BOTH);
-					if(!password_verify( $pwd ,$row2[0])){
+		$account_T = substr($account,0,1);		
+		switch($account_T){
+			case 'D':
+				try{
+					$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
+					$cusr=$db->query("SELECT Student_id FROM student WHERE Student_id = '$account';");
+					$row1=$cusr->fetch(PDO::FETCH_BOTH);
+					if(empty($row1[0])){
+						$db=null;
 						print<<<_END
 						<script>
-						alert ("使用者名稱或密碼錯誤");
+						alert ("使用者不存在");
 						setTimeout(function(){window.location.href='login.php';},1000);
 						</script>
 						_END;
-					}else{
-						session_start();
-						$_SESSION['account'] = $account;
-						$_SESSION['pwd'] = $pwd;
-						header("refresh:0;url=welcome.php");
+					}else {
+						$cpwd=$db->query("SELECT Password FROM student WHERE Student_id = '$account';");
+						$row2=$cpwd->fetch(PDO::FETCH_BOTH);
+						if(!password_verify( $pwd ,$row2[0])){
+							print<<<_END
+							<script>
+							alert ("使用者名稱或密碼錯誤");
+							setTimeout(function(){window.location.href='login.php';},1000);
+							</script>
+							_END;
+						}else{
+							session_start();
+							$_SESSION['account'] = $account;
+							$_SESSION['pwd'] = $pwd;
+							header("refresh:0;url=welcome.php");
+							$db=null;
+							exit;
+						}
 						$db=null;
-						exit;
 					}
-					$db=null;
+				}catch (PODException $e){
+					print "couldn't to connect to db " . $e->getMessage();
 				}
-			}catch (PODException $e){
-				print "couldn't to connect to db " . $e->getMessage();
-			}
-		}
-		function Teacher(){
-			try{
+			break;
+			case 'T':
+				try{
+					$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
+					$cusr=$db->query("SELECT Password FROM admin WHERE Admin_id = '$account';");
+					$row=$cusr->fetch(PDO::FETCH_BOTH);
+				}catch (PODException $e){
+					print "couldn't to connect to db " . $e->getMessage();
+				}
+			break;
+			case 'A':
 				$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
 				$cusr=$db->query("SELECT Password FROM admin WHERE Admin_id = '$account';");
 				$row=$cusr->fetch(PDO::FETCH_BOTH);
-			}catch (PODException $e){
-				print "couldn't to connect to db " . $e->getMessage();
-			}
-		}
-		function Admin(){
-			$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
-			$cusr=$db->query("SELECT Password FROM admin WHERE Admin_id = '$account';");
-			$row=$cusr->fetch(PDO::FETCH_BOTH);
-			if($pwd == $row['Password']){
-				session_start();
-				$_SESSION['A_account'] = $account;
-				$_SESSION['A_pwd'] = $pwd;
-				header("refresh:0;url=admin\admin.php");
-				$db=null;
-			}else{
+				if($pwd == $row['Password']){
+					session_start();
+					$_SESSION['A_account'] = $account;
+					$_SESSION['A_pwd'] = $pwd;
+					header("refresh:0;url=admin\admin.php");
+					$db=null;
+				}else{
 				print<<<END
 					<script>
 					alert ("使用者名稱或密碼錯誤");
 					setTimeout(function(){window.location.href='login.php';},1000);
 					</script>
 				END;
-			}
-		}
-		switch($account_T){
-			case 'D':
-				Student();
-			break;
-			case 'T':
-				Teacher();
-			break;
-			case 'A':
-				Admin();
+				}
 			break;
 		}
 	}else{
