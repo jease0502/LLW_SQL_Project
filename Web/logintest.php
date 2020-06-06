0c6d00e65555003988eb14bd4487f8270932099b
ftsjease <ftsjease0502@gmail.com>
2020-06-07 04:01:41 +0800

    logintest

--- Web/logintest.php
+++ Web/logintest.php
@@ -43,10 +43,10 @@
 				$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
 				$cusr=$db->query("SELECT Password FROM admin WHERE Admin_id = '$account';");
 				$row=$cusr->fetch(PDO::FETCH_BOTH);
-				if($pwd == $row[Password]){
+				if($pwd == $row['Password']){
 					session_start();
-					$_SESSION[A_account] = $account;
-					$_SESSION[A_pwd] = $pwd;
+					$_SESSION['A_account'] = $account;
+					$_SESSION['A_pwd'] = $pwd;
 					header("refresh:0;url=admin\admin.php");
 					$db=null;
 				}else{
