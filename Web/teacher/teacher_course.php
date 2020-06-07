ca870b89c53e98f8ac1d5dfaa34af79304bd759d
wuminyen <mashirorin1211@gmail.com>
2020-06-08 03:57:44 +0800

    update

--- Web/teacher/teacher_course.php
+++ Web/teacher/teacher_course.php
@@ -1,3 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>教師課程內容編輯</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+
+<?php
+session_start();
+$T_id = $_SESSION['T_account'];
+require_once("conect.php");
+$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
+$q = "SELECT class_detail.Code FROM class_detail INNER JOIN teacher ON teacher.Name = class_detail.Teacher_Name and teacher.Teacher_id = '$T_id'";
+$query = $db->query($q);
+$datalist = $query->fetchAll();
+foreach ($datalist as $datainfo)
+	    	{
+		       //echo $datainfo[0];
+		       //echo "<br />";
+	    	}
+?>
+
+<body>
+	<h1>教師課程內容編輯</h1>
+	<form name="myForm" action="course_view.php" method="post" >
+		<h5 >請輸入課程代碼</h5>
+		<input  align="center" type="submit" name="value" value="提交" />
+		<select  name="Code">
+            <?php
+            foreach ($datalist as $datainfo)
+            	{
+            		print "<option value='$datainfo[0]'>$datainfo[0]</option>";
+            	}
+
+            ?>
+        </select>
+	</form>
+<input type ="button" onclick="history.back()" value="回到上一頁">
+</body>
+</html>
--- Web/teacher/teacher_home.php
+++ Web/teacher/teacher_home.php
@@ -1,15 +1,68 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>教師管理頁面</title>
+<style>
+		#topContent {
+			background-image: url('./Images/login_banner.jpg');
+			background-repeat: no-repeat;
+			text-align: right;
+			background-color: #006699;
+			color: White;
+			font-size: x-large;
+			text-decoration: none;
+			font-weight: bold;
+			padding: 10px;
+			height: 75px;
+			min-width: 970px;	
+		}
+		.button {
+			display: inline-block;
+			text-align: center;
+			vertical-align: middle;
+			padding: 12px 25px;
+			border: 1px solid #3c3939;
+			border-radius: 8px;
+			background: #726b6b;
+			background: -webkit-gradient(linear, left top, left bottom, from(#726b6b), to(#3c3939));
+			background: -moz-linear-gradient(top, #726b6b, #3c3939);
+			background: linear-gradient(to bottom, #726b6b, #3c3939);
+			-webkit-box-shadow: #898080 0px 0px 40px 0px;
+			-moz-box-shadow: #898080 0px 0px 40px 0px;
+			box-shadow: #898080 0px 0px 40px 0px;
+			text-shadow: #242222 1px 1px 1px;
+			font: normal normal bold 20px times new roman;
+			color: #ffffff;
+			text-decoration: none;
+		}
+		.button:hover,
+		.button:focus {
+			border: 1px solid #474343;
+			background: #898080;
+			background: -webkit-gradient(linear, left top, left bottom, from(#898080), to(#484444));
+			background: -moz-linear-gradient(top, #898080, #484444);
+			background: linear-gradient(to bottom, #898080, #484444);
+			color: #ffffff;
+			text-decoration: none;
+		}
+		.button:active {
+			background: #3c3939;
+			background: -webkit-gradient(linear, left top, left bottom, from(#3c3939), to(#3c3939));
+			background: -moz-linear-gradient(top, #3c3939, #3c3939);
+			background: linear-gradient(to bottom, #3c3939, #3c3939);
+		}		
+</style>
+	<title>教師管理頁面</title>
 </head>
-
-<h1 align="middle">教師管理頁面</h1>
-<table >
+<div id="topContent">
+<h1>教師管理頁面</h1>
+</div>
+<table>
 	<tr>
-		<input type="button" value="課程編輯" onclick="location.href='teacher_course.php'">
-		<input type="button" value="課程選課狀態" onclick="location.href='teacher_course_status.php'">
-		<input type="button" value="教師資料編輯" onclick="location.href='teacher_contact.php'">
-	</tr>
+		<td><input type="button" class="button" value="課程編輯" onclick="location.href='teacher_course.php'"></td>
+	<tr>
+		<td><input type="button" class="button" value="課程選課狀態" onclick="location.href='teacher_course_status.php'"></td>
+	<tr>
+		<td><input type="button" class="button" value="教師資料編輯" onclick="location.href='teacher_contact.php'"></td>
+	<tr>
 </table>
 </html>
