3e3ffe0ff57535fde40e892d4b7e1266d772ef99
ftsjease <ftsjease0502@gmail.com>
2020-06-08 01:02:40 +0800

    admin test

--- Web/teacher/teacher_course.php
+++ Web/teacher/teacher_course.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-	<title>資料修改</title>
+	<title>教師課程內容編輯</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -15,6 +14,6 @@
 </head>
 
 <body>
-
+	<h1>教師課程內容編輯</h1>
 </body>
 </html>
